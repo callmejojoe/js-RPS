0de66b4d73d7db474da0df0d1ceb6389531e0658
callmejojoe <legendaryjoezy@gmail.com>
2026-07-13 19:20:23 +0100

    used to math.random method to make a random number...or random choice generator and I tested it out by linking it to a button

--- app.js
+++ app.js
@@ -3,40 +3,38 @@ console.log("game start");
 const mainPage = document.getElementById("mainPage");
 mainPage.innerHTML = `
 <h1 id= head > Rock Paper Scissors (js btw) </h1>
-<ul id= list ></ul>
-<button id= 'prmtp' >click me</button>
+<button id= 'startGame' >click me</button>
 <div id= txt></div>
 `
-const list = document.getElementById("list");
-list.innerHTML = `
-<li> tehe </li>
-`
-function getPlayerchoice() {
-let choice = prompt("say smt twink", "like this shii");
-	const txt = document.getElementById("txt");
+const txt = document.getElementById("txt");
+function gRint(max) {
+  let rand = Math.floor(Math.random() * max);
 	txt.innerHTML = `
-            <h2 id= choicetxt > ${choice} </h2>
-	`;
-	console.log(choice);
-	return(choice);
+	<p1> ${rand} </p1>
+`
 }
-
-const prmptTest = document.getElementById('prmtp');
-prmptTest.addEventListener("click", () => {getPlayerchoice()})
+const startGame = document.getElementById('startGame');
+startGame.addEventListener("click", () => {gRint(3)})
 //ignore, just for fun
 //psuedo code
 //score board is created
 let playerScore = 0;
 let compScore = 0;
+const handOptions = ["rock", "paper", "scissors"]
 //max score is decided
 let maxScore = 3;
 //game starts {a loop that continues till either player reaches the max score}
-function gameStart() {}
+function gameStart() {
 //player inputs their choice of attack (rock, paper, scissors)
+	const getPlayerChoice = prompt("tis a game of rock paper scissors, type your hand")
 //computer generates a random choice to go against the player
+	const getCompChoice = () => {}
 //a rule() funtion takes the inputs as parameters 
+	function gameRule(playerChoice, compChoice) {
 //the rule() funtion updates the score board based on who wins that round
+	}
 //game ends when a player reaches the max score
+}
 //celebrate the winning player
 
 
